b27fa0636e58307bed96b0c23c257f35a5f6cc48
insookim0702 <insookim0702@gmail.com>
2019-01-19 01:44:00 +0900

    add camera function, write, word register, saving image in to database

---
+++ app/src/main/java/com/example/kinso/testmarket/ItemInfo.java
@@ -0,0 +1,15 @@
+package com.example.kinso.testmarket;
+
+/**
+ * Created by kinso on 2018-12-25.
+ */
+
+public class ItemInfo {
+    public int drawableId;
+    public String price;
+
+    public ItemInfo(int drawableId, String price){
+        this.drawableId = drawableId;
+        this.price = price;
+    }
+}
